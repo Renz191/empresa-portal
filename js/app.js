24dc0e5cf5b80c71d3a33a8c9d4ba67d4ba793cc
Renz191 <renanfvianna1@gmail.com>
2026-01-16 15:47:23 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+console.log('Renan Portfolio Loaded');
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
